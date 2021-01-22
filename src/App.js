7a09540be3ae30985174de613960a690bcbd4874
elisagiorgi <77390165+elisagiorgi@users.noreply.github.com>
2021-01-22 15:25:53 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./styles.css";
+import TodoApp from "./TodoApp.js";
+export default function App() {
+  return (
+    <div>
+      <TodoApp />
+    </div>
+  );
+}
