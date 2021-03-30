1c42df720c4f0bd96c3a9a948e2ee3a7e897b56c
Elisa Giorgi <e.giorgi@teamsystem.com>
2021-03-30 11:18:41 +0200

    Updates

---
+++ precache-manifest.31f9c38af24d2082b2633b00d885f484.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1635dd919815c0113065d4f045e5aa00",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "3e1bef53ce4da4a74419",
+    "url": "/todolist/static/css/2.45ad7e77.chunk.css"
+  },
+  {
+    "revision": "306245bc250148fbcabc",
+    "url": "/todolist/static/css/main.11eba86e.chunk.css"
+  },
+  {
+    "revision": "3e1bef53ce4da4a74419",
+    "url": "/todolist/static/js/2.df306587.chunk.js"
+  },
+  {
+    "revision": "406ecc7272e65f6d892b81a2105d3b93",
+    "url": "/todolist/static/js/2.df306587.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "306245bc250148fbcabc",
+    "url": "/todolist/static/js/main.7f50d26e.chunk.js"
+  },
+  {
+    "revision": "197f51c8578d731c6831",
+    "url": "/todolist/static/js/runtime-main.e7725399.js"
+  }
+]);
